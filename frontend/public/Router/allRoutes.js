22fe57b4046f9e99074472b8fc68a5abfdcb9c88
nelly34800 <nellyboussekane@free.fr>
2026-02-02 14:59:21 +0100

    création des pages home et contact et ajout des photos

--- frontend/public/Router/allRoutes.js
+++ frontend/public/Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/cgv", "CGV", "/pages/cgv.html"),
+    new Route("/mentions", "Mentions légales", "/pages/mentions.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Vite & Gourmand";
