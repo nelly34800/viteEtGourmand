65825919fe28da2dcf88ce4b98e50b2b7097d699
nelly34800 <nellyboussekane@free.fr>
2026-07-08 20:10:23 +0200

    modification docker pour variable mongo composer.lock enlevé du gitignore

--- backend/config/mongodb.php
+++ backend/config/mongodb.php
@@ -2,8 +2,8 @@
 // import de la classe Client de la bibliothèque MongoDB
 use MongoDB\Client;
 // création de la connexion
-$mongo = $_ENV['MONGODB_PORT'] ?? null;
+$mongoUrl = $_ENV['MONGODB_URL'] ?? 'mongodb://mongo:27017';
 
-$mongoClient = new Client("mongodb://mongo:27017");
+$mongoClient = new Client($mongoUrl);
 // création de la base au 1er insert
 $mongoDb = $mongoClient->selectDatabase("app_statistics");
