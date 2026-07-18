52e6f9507cbd817a09bbda4b7462c20195cf669b
nelly34800 <nellyboussekane@free.fr>
2026-07-18 16:48:25 +0200

    test sans le middleware sur les statistic.routes;php

--- backend/Routes/statistic.routes.php
+++ backend/Routes/statistic.routes.php
@@ -6,11 +6,11 @@ use App\Middleware\AuthMiddleware;
 $allowedRoles = ['admin'];
 
 $router->get('/statistics/ordersByMenu', function() use ($allowedRoles) {
-    AuthMiddleware::requireRole(['admin']);
+    //AuthMiddleware::requireRole(['admin']);
     (new StatisticsController())->ordersByMenu();
 });
 
 $router->get('/statistics/revenueByMenu', function() use ($allowedRoles) {
-    AuthMiddleware::requireRole($allowedRoles);
+    //AuthMiddleware::requireRole($allowedRoles);
     (new StatisticsController())->revenueByMenu();
 });
