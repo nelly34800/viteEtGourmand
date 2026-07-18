d398919d8a289894b465f24108baa1a14c0d5e6a
nelly34800 <nellyboussekane@free.fr>
2026-07-18 16:32:02 +0200

    suppression du test des routes de statistiques

--- backend/Routes/statistic.routes.php
+++ backend/Routes/statistic.routes.php
@@ -6,12 +6,8 @@ use App\Middleware\AuthMiddleware;
 $allowedRoles = ['admin'];
 
 $router->get('/statistics/ordersByMenu', function() use ($allowedRoles) {
-   // On commente temporairement l'auth et le contrôleur et renvoie text brut
-    // AuthMiddleware::requireRole(['admin']);
-    // (new StatisticsController())->ordersByMenu();
-    
-    echo json_encode(["message" => "La route fonctionne sur Heroku !"]);
-    exit;
+    AuthMiddleware::requireRole(['admin']);
+    (new StatisticsController())->ordersByMenu();
 });
 
 $router->get('/statistics/revenueByMenu', function() use ($allowedRoles) {
