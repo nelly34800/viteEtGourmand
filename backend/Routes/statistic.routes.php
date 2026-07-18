8c15c855969b7cc14f05581c4644fa2215557a30
nelly34800 <nellyboussekane@free.fr>
2026-07-18 17:47:19 +0200

    test avec les routes de statistic en minuscule

--- backend/Routes/statistic.routes.php
+++ backend/Routes/statistic.routes.php
@@ -5,12 +5,12 @@ use App\Middleware\AuthMiddleware;
 
 $allowedRoles = ['admin'];
 
-$router->get('/statistics/ordersByMenu', function() use ($allowedRoles) {
-    //AuthMiddleware::requireRole(['admin']);
+$router->get('/statistics/ordersbymenu', function() use ($allowedRoles) {
+    AuthMiddleware::requireRole(['admin']);
     (new StatisticsController())->ordersByMenu();
 });
 
-$router->get('/statistics/revenueByMenu', function() use ($allowedRoles) {
-    //AuthMiddleware::requireRole($allowedRoles);
+$router->get('/statistics/revenuebymenu', function() use ($allowedRoles) {
+    AuthMiddleware::requireRole($allowedRoles);
     (new StatisticsController())->revenueByMenu();
 });
